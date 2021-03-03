0f951a0d486b505d282a06f1468ba3bca310cd63
Ethan Song <600020389@fjuhsd.org>
2021-03-03 17:36:57 +0000

    Wrote the setup

---
+++ Loan.java
@@ -0,0 +1,16 @@
+//Programmers: Ethan Song and Quinn Cassady
+
+import java.util.ArrayList;
+
+public class Loan{
+  //instance variables 
+
+  //constructor
+
+  //accessor methods
+
+  //modifier methods
+
+  //toString method 
+
+}
