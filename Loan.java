6df60cb0fb15c13e372e528aef6b5cc403077a17
Ethan Song <600020389@fjuhsd.org>
2021-03-04 19:46:02 +0000

    Finished variables, constructor, accessor and modifier methods.

--- Loan.java
+++ Loan.java
@@ -4,13 +4,73 @@ import java.util.ArrayList;
 
 public class Loan{
   //instance variables 
-
+  private int ID; 
+  private double loanAmount;
+  private String country;
+  private int daysToFund;
+  int numLenders;
+  
   //constructor
+  public Loan(int ID, double loanAmount, String country, int daysToFund, int numLenders)
+  {
+    this.ID = ID;
+    this.loanAmount = loanAmount;
+    this.country = country;
+    this.daysToFund = daysToFund;
+    this.numLenders = numLenders;
+  }
 
   //accessor methods
+  public int getID()
+  {
+    return ID;
+  }
+
+  public double getLoanAmount()
+  {
+    return loanAmount;
+  }
+
+  public String getCountry()
+  {
+    return country;
+  }
+
+  public int getDaysToFund()
+  {
+    return daysToFund;
+  }
+
+  public int getNumLenders()
+  {
+    return numLenders;
+  }
 
   //modifier methods
-
+  public void setID(int newID)
+  {
+    ID = newID;
+  }
+  public void setLoanAmount(double newAmount)
+  {
+    loanAmount = newAmount;
+  }
+  public void setCountry(String newCountry)
+  {
+    country = newCountry;
+  }
+  public void setDaysToFund(int newDays)
+  {
+    daysToFund = newDays;
+  }
+  public void setNumLenders(int newNum)
+  {
+    numLenders = newNum;
+  }
+  
   //toString method 
-
+  public String toString()
+  {
+    return "ID: " + ID + " loan amount: " + loanAmount + " country: " + country + " days to fund: " + daysToFund + " num lenders: " + numLenders;
+  }
 }
